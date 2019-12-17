b0be8ca8488220cdf937f5b71d139bf18301e020
Elton Fonseca <elton869@gmail.com>
2019-12-16 21:22:50 -0300

    06 - HTML e PHP

---
+++ pagina.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Minha primeira página com PHP</h1>
+
+    <?php echo "Minha string vinda do PHP"; ?>
+
+    <p>Estou no curso de <?php echo "PHP"; ?></p>
+
+    <p>10 mais 10 é igual a <?php echo 10 + 10; ?></p>
+
+    <?php
+        echo "minha linha 1 <br>";
+
+        echo "minha linha 2 <br>";
+
+        echo "minha linha 3";
+    ?>
+</body>
+</html>
