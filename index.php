0588e9dc8095fc2e6efdcf02b4c771e9f1f2049e
Elton Fonseca <elton869@gmail.com>
2019-12-17 15:32:05 -0300

    11 - Comentários no PHP

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-phpinfo();
+// Essa linha será completamente ignorada
+
+# Essa linha também será ignorada
+
+phpinfo(); //Isso será ignorado, mas o phpinfo não!
+
+/*
+ *   Essa linha será ignorada
+ *
+ *   Essa linha também é um comentário
+ *
+ *   E por ai vai...
+*/
+
+// echo "Treinaweb";
+// echo "Treinaweb";
+// echo "Treinaweb";
+// echo "Treinaweb";
+
+echo "Treinaweb";
+echo "Treinaweb";
+echo "Treinaweb";
+
+
