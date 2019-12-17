77691cb3c33d8cdb9964daab7c0ed6146925bade
Elton Fonseca <elton869@gmail.com>
2019-12-17 18:14:00 -0300

    14 - Arrays Associativos

--- arrays.php
+++ arrays.php
@@ -4,7 +4,22 @@ $linguagens = ["PHP", "C#", "Java"];
 
 $linguagens2 = array("PHP", "C#", "Java");
 
-echo $linguagens2[1];
+$curso = ["Curso de PHP Fundamentos", 7.4, 40, true];
+
+$curso = [
+    "nome_curso" => "Curso de PHP Fundamentos",
+    "versao_ferramenta" => 7.4,
+    "carga_horaria" => 40,
+    "status" => true
+];
+
+echo $curso["nome_curso"];
+echo "<br>";
+echo $curso["versao_ferramenta"];
+echo "<br>";
+echo $curso["carga_horaria"];
+echo "<br>";
+echo $curso["status"];
 
 
 
