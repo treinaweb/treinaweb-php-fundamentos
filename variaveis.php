3c069323d2580e03a36f30beedfb5de9070b320f
Elton Fonseca <elton869@gmail.com>
2019-12-17 14:06:01 -0300

    09 - Utilizando variáveis

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php
+
+$email = "elton.fonseca@treinaweb.com.br";
+
+echo $email;
+
+$email = "elton869@gmail.com";
+
+echo "<br>";
+echo $email;
+
+$email = 100;
+
+echo "<br>";
+echo $email;
+
+$nome = "Elton Fonseca";
+
+echo "<br>";
+echo $nome;
