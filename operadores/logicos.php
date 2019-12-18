ed1521aa48d8f61be4dbe8bedabfc5c0dca01e83
Elton Fonseca <elton869@gmail.com>
2019-12-18 15:18:31 -0300

    19 - Operadores lógicos

---
+++ operadores/logicos.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = true;
+$b = false;
+
+// && and
+// var_dump($a && $b);
+// var_dump($a and $b);
+
+// || or
+// var_dump($a || $b);
+// var_dump($a or $b);
+
+// xor
+//var_dump($a xor $b);
+
+// !
+var_dump(!$b);
