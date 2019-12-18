46a139897c0efcfd6c24740e5876759acfaf8557
Elton Fonseca <elton869@gmail.com>
2019-12-18 10:47:35 -0300

    17 - Operadores de comparação

---
+++ operadores/comparadores.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+var_dump($a == $b);
+echo "<br>";
+
+var_dump($a != $b);
+echo "<br>";
+
+var_dump($a <> $b);
+echo "<br>";
+
+var_dump($a < $b);
+echo "<br>";
+
+var_dump($a > $b);
+echo "<br>";
+
+var_dump($a <= $b);
+echo "<br>";
+
+var_dump($a >= $b);
+echo "<br>";
