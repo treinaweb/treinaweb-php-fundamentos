0f3aae1f6baf6623652249fbc691a7ddcfaa465d
Elton Fonseca <elton869@gmail.com>
2019-12-18 10:01:53 -0300

    16 - Operadores Aritméticos

---
+++ operadores.php
@@ -0,0 +1,29 @@
+<?php
+
+$soma = 5 + 6.6;
+$subtracao = 10 - 5.5;
+
+$multiplicacao = 5 * 10.6;
+$divisao = 100 / 9;
+
+$resto = 20 % 3;
+$exponencial = 10 ** 3;
+
+var_dump($soma);
+echo "<br>";
+
+var_dump($subtracao);
+echo "<br>";
+
+var_dump($multiplicacao);
+echo "<br>";
+
+var_dump($divisao);
+echo "<br>";
+
+var_dump($resto);
+echo "<br>";
+
+var_dump($exponencial);
+echo "<br>";
+
