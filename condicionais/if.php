458b8d4a2c104d493a76f147c2a0b1ef3088de9b
Elton Fonseca <elton869@gmail.com>
2019-12-19 13:59:42 -0300

    22 - Condicional IF

---
+++ condicionais/if.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 30;
+$b = 20;
+
+// if ($a > $b) {
+//     echo "Realmente A e maior do que B <br>";
+//     echo "segunda linha<br>";
+//     echo "terceira linha<br>";
+// }
+
+if ($a > $b) {
+    echo "Realmente A e maior do que B <br>";
+}
+    
