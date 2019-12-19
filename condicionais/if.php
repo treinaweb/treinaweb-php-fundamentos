3790f75f2687d83d05dd1ae027f6cf5e9ce620fc
Elton Fonseca <elton869@gmail.com>
2019-12-19 14:16:11 -0300

    23 - Trabalhando com elseif e else.mov

--- condicionais/if.php
+++ condicionais/if.php
@@ -1,15 +1,15 @@
 <?php
 
-$a = 30;
-$b = 20;
-
-// if ($a > $b) {
-//     echo "Realmente A e maior do que B <br>";
-//     echo "segunda linha<br>";
-//     echo "terceira linha<br>";
-// }
+$a = 11;
+$b = 21;
 
 if ($a > $b) {
     echo "Realmente A e maior do que B <br>";
+} elseif ($b == 20) {
+    echo "B e igual a 20";
+} elseif ($a == 10) {
+    echo "A e igual a 10";
+} else {
+    echo "Nenhuma condicao e verdadeira";
 }
     
