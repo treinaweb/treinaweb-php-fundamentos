78dc11f811e7b2f732de40d39f763aaf343b261c
Elton Fonseca <elton869@gmail.com>
2019-12-20 14:09:26 -0300

    27 - Loop Foreach

--- loops/for.php
+++ loops/for.php
@@ -1,5 +1,11 @@
 <?php
 
-for ($i=0; $i < 20; $i=$i+2) {
-    echo "executou $i <br>";
+// for ($i=0; $i < 20; $i=$i+2) {
+//     echo "executou $i <br>";
+// }
+
+$numeros = [2, 3, 5, 7, 9, 10];
+
+for ($i=0; $i < count($numeros); $i=$i+1) { 
+    echo $numeros[$i] . "<br>";
 }
---
+++ loops/foreach.php
@@ -0,0 +1,15 @@
+<?php
+
+$numeros = [2, 3, 5, 7, 9, 10];
+
+// foreach ($numeros as $chave => $valor) {
+//     echo "chave: $chave - valor : $valor <br>";
+// }
+
+// foreach ($numeros as $chave => $valor) {
+//     echo "chave: $chave - valor : {$numeros[$chave]} <br>";
+// }
+
+foreach ($numeros as $valor) {
+    echo "valor : {$valor} <br>";
+}
