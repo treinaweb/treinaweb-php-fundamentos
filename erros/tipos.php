103288b5e33f83fc5c3a13d4a9749934fa2bea25
Elton Fonseca <elton869@gmail.com>
2019-12-27 17:42:18 -0300

    32 - Principais tipos de erros do PHP

---
+++ erros/tipos.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Notice
+// $valor1 = 10;
+// echo $valor;
+
+// Warning
+//$numero = NAO_EXISTE;
+
+// Fatal
+function somar($valor1, $valor2) {
+    return $valor2 + $valor1;
+}
+
+// function somar($valor1, $valor2) {
+//     return $valor2 + $valor1;
+// }
+
+// Parse error
+// echo "asdadsd"
+
+echo "chegou ao fim do programa";
